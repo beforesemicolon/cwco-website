13a389300faed3f3fa9cd8156aa590116453ffb1
Elson Correia <design@elsoncorreia.com>
2022-02-13 20:14:04 -0500

    setup all blank pages and style documentation menu

--- pages/data/pages.js
+++ pages/data/pages.js
@@ -1,25 +1,74 @@
 const path = require("path");
 const fs = require("fs");
 
-module.exports = {
-  "/": {
-    title: (locale) => locale.pages.home.title,
-    description: (locale) => locale.pages.home.description,
-    url: "/",
+const createPage = (url, {localeKey, template, imageUrl = ""} = {}) => {
+  return {
+    title: (locale) => locale.pages[localeKey].title,
+    description: (locale) => locale.pages[localeKey].description,
+    url,
     CSP: "script-src \"nonce-local\", \"strict-dynamic\" \"unsafe-inline\" https:; object-src \"none\"; base-uri http://localhost:3000/;",
-    imageUrl: ""
-  },
-  "/documentation": {
-    title: (locale) => locale.pages.documentation.title,
-    description: (locale) => locale.pages.documentation.description,
-    url: "/documentation",
-    CSP: "script-src \"nonce-local\", \"strict-dynamic\" \"unsafe-inline\" https:; object-src \"none\"; base-uri http://localhost:3000/;",
-    imageUrl: "",
+    imageUrl,
     content: (locale) => {
-      return fs.readFileSync(
-        path.resolve(__dirname, `./documentations/${locale}/why-cwco.html`),
-        'utf-8'
-      );
+      return template
+        ? fs.readFileSync(
+          path.resolve(__dirname, `./documentations/${locale}/${template}.html`),
+          'utf-8'
+        )
+        : '';
     }
-  }
+  };
+}
+
+module.exports = {
+  "/": createPage("/", {localeKey: "home"}),
+  "/documentation": createPage("/documentation", {localeKey: "documentation", template: "why-cwco"}),
+  "/documentation/getting-started": createPage("/documentation/getting-started", {
+    localeKey: "gettingStarted",
+    template: "getting-started"
+  }),
+  "/documentation/web-component": createPage("/documentation/web-component", {
+    localeKey: "webComponent",
+    template: "web-component"
+  }),
+  "/documentation/context-provider-component": createPage("/documentation/context-provider-component", {
+    localeKey: "contextProviderComponent",
+    template: "context-provider-component"
+  }),
+  "/documentation/configurations": createPage("/documentation/configurations", {
+    localeKey: "configurations",
+    template: "configurations"
+  }),
+  "/documentation/on-mount": createPage("/documentation/on-mount", {localeKey: "onMount", template: "on-mount"}),
+  "/documentation/on-destroy": createPage("/documentation/on-destroy", {
+    localeKey: "onDestroy",
+    template: "on-destroy"
+  }),
+  "/documentation/on-update": createPage("/documentation/on-update", {localeKey: "onUpdate", template: "on-update"}),
+  "/documentation/on-adoption": createPage("/documentation/on-adoption", {
+    localeKey: "onAdoption",
+    template: "on-adoption"
+  }),
+  "/documentation/on-error": createPage("/documentation/on-error", {localeKey: "onError", template: "on-error"}),
+  "/documentation/attributes": createPage("/documentation/attributes", {
+    localeKey: "attributes",
+    template: "attributes"
+  }),
+  "/documentation/properties": createPage("/documentation/properties", {
+    localeKey: "properties",
+    template: "properties"
+  }),
+  "/documentation/template": createPage("/documentation/template", {localeKey: "template", template: "template"}),
+  "/documentation/directive": createPage("/documentation/directive", {localeKey: "directive", template: "directive"}),
+  "/documentation/custom-directive": createPage("/documentation/custom-directive", {
+    localeKey: "customDirective",
+    template: "custom-directive"
+  }),
+  "/documentation/context": createPage("/documentation/context", {localeKey: "context", template: "context"}),
+  "/documentation/stylesheet": createPage("/documentation/stylesheet", {
+    localeKey: "stylesheet",
+    template: "stylesheet"
+  }),
+  "/documentation/events": createPage("/documentation/events", {localeKey: "events", template: "events"}),
+  "/documentation/errors": createPage("/documentation/errors", {localeKey: "errors", template: "errors"}),
+  "/documentation/faq": createPage("/documentation/faq", {localeKey: "faq", template: "faq"}),
 }
--- server.js
+++ server.js
@@ -5,16 +5,30 @@ const {engine} = require('@beforesemicolon/html-plus');
 
 const app = express();
 
+const getRequestData = req => {
+  const locale = req.acceptsLanguages("en");
+  const path = req.path.length > 1 ? req.path.replace(/(\/|\/index\.html)$/, '') : req.path;
+  return {
+    path,
+    locale,
+    params: req.params
+  }
+}
+
 (async () => {
+  app.get('/documentation/:doc?', (req, res, next) => {
+    const ext = path.extname(req.path);
+    
+    if (!ext || ext === '.html') {
+      return res.render('documentation', getRequestData(req));
+    }
+    
+    next();
+  })
+  
   await engine(app, path.resolve(__dirname, './pages'), {
     onPageRequest(req) {
-      const locale = req.acceptsLanguages("en");
-      const path = req.path.length > 1 ? req.path.replace(/(\/|\/index\.html)$/, '') : req.path;
-      return {
-        path,
-        locale,
-        params: req.params
-      }
+      return getRequestData(req)
     }
   });
   
